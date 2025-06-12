8e5ab43ce3a73ad002375f2d6014bb915ac007ff
Prathameshh12 <prathamesh.ahire1204@gmail.com>
2025-06-12 23:09:58 +1000

    Initial commit

---
+++ ColourPhoto/ColourPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColourPhotoApp.swift
+//  ColourPhoto
+//
+//  Created by Prathamesh Ahire on 12/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct ColourPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColourPhotoTests/ColourPhotoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColourPhotoTests.swift
+//  ColourPhotoTests
+//
+//  Created by Prathamesh Ahire on 12/6/2025.
+//
+
+import Testing
+@testable import ColourPhoto
+
+struct ColourPhotoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
